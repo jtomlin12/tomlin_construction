f28f4e881ff18796062aefbcedcda1ed86b79492
Justin Tomlin <jtomlin12@yahoo.com>
2020-07-19 23:20:57 -0400

    cimmit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,34 @@
 import React from "react";
 import { Container } from "reactstrap";
-// import { Fade } from 'react-slideshow-image';
-// import 'react-slideshow-image/dist/styles.css'
+import { Fade } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+import img1 from "../images/IMG_3187.JPG"
+import img2 from "../images/IMG_3185.JPG"
+import img3 from "../images/IMG_3186.JPG"
+import img4 from "../images/IMG_1974.JPG"
 
 export default function Home(props) {
   const fadeImages = [
-    "/images/IMG_3178.JPG",
-    "/images/IMG_3185.JPG",
-    "/images/IMG_3186.JPG",
-    "/images/IMG_3184.JPG",
-    "/images/IMG_1974.JPG"
+    img1,
+    img2,
+    img3,
+    img4,
   ];
 
   return (
     <div>
       <Container>
-      {/* <div className="slide-container">
+      <div className="slide-container">
         <Fade>
-          <div className="each-fade">
-            <img src={fadeImages[0]} alt="image0" className="App"/>
-          </div>
-          <div className="each-fade">
-            <img src={fadeImages[1]} alt="image1" className="App" />
-          </div>
-          <div className="each-fade">
-            <img src={fadeImages[2]} alt="image2" className="App" />
-          </div>
-          <div className="each-fade">
-            <img src={fadeImages[3]} alt="image3" className="App" />
-          </div>
-          <div className="each-fade">
-            <img src={fadeImages[4]} alt="image4" className="App" />
-          </div>
+          {
+            fadeImages.map((img, i) => (
+              <div key={i} className="each-fade">
+                <img src={img} alt="image0" className="App"/>
+              </div>
+            ))
+          }
         </Fade>
-      </div> */}
+      </div>
       </Container>
     </div>
   );
